155d1eefc77b53b764f15ae66d3e2eea0530bf13
sjm446 <sjm446@gmail.com>
2021-05-17 15:50:16 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ echo Generating Maze
 python amazed.py --algo wilson solution-grid 10 20 0 0 9 19 > maze.txt
 cat maze.txt
 echo Uploading Maze to S3
-echo AWS_S3_BUCKET is set to $AWS_S3_BUCKET
+echo EXPORT_S3_BUCKET_URL is set to $EXPORT_S3_BUCKET_URL
 python upload.py
