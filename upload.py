bfc7a27fafb851441df76156e063e572ef619c20
sjm446 <sjm446@gmail.com>
2021-05-17 15:49:32 +0100

    Update upload.py

--- upload.py
+++ upload.py
@@ -8,4 +8,4 @@ if (BUCKET != None):
     with open("maze.txt", "rb") as f:
         s3.upload_fileobj(f, BUCKET, "maze"+str(random.randrange(100000))+".txt")
 else:
-    print("AWS_S3_BUCKET was not set so not uploading file")
+    print("EXPORT_S3_BUCKET_URL was not set so not uploading file")
